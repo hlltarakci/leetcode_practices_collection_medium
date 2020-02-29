1f463705676d77ce4df6b4f55efa079e8d1fd155
Hilal Tarakci <hlltarakci@gmail.com>
2020-02-29 20:40:42 +0300

    add solution

---
+++ code/treesAndGraphs/populatingNextRightPointersInEachNode.java
@@ -0,0 +1,49 @@
+ // https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+class Solution {
+    /*
+        time: O(n)
+        space: O(log n) -- recursion stack
+    */
+    public Node connect(Node root) {
+        connect(null, root);
+        return root;
+    }
+    
+    private void connect(Node parent, Node root) {
+        if(root == null) {
+            return;
+        } else if(parent == null) {
+            root.next = null;
+        } else if(parent.right != root) {
+            root.next = parent.right;
+        } else {
+            root.next = parent.next != null ? parent.next.left : null;
+        }
+        
+        connect(root, root.left);
+        connect(root, root.right);
+    }
+}
